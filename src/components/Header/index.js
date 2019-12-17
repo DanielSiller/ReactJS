03be404a5991a939e3e857aa89f0750ff245fecb
Daniel Siller Thebaldi <57238642+DanielSiller@users.noreply.github.com>
2019-12-17 16:46:26 -0300

    Curso Starter
    React JS

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => 
+    <header id = 'main-header'>
+           <a href = '/' className = 'button'>JSHunt</a>
+    </header>;
+
+export default Header;
